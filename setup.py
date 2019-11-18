527407c64520a0d042c0df6308d614b620762aa0
Anita Joe <achukutty.jose@gmail.com>
2019-11-18 04:07:17 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ executables = [cx_Freeze.Executable("TicTacToe.py")]
 cx_Freeze.setup(
     name="TicTacToe",
     options={"build_exe": {"packages":["pygame"]
-                           }},
+                           "include_files":["python.png"]}},
     executables = executables
 
     )
